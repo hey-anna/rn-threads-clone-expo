691aa54dc105942c39b16ab5f2e2940aa18c91f6
hey-anna <106728222+hey-anna@users.noreply.github.com>
2025-05-20 23:48:21 +0900

    feat: Login 페이지 추가 및 RN 전체 레이아웃 개념 주석 추가
    - Login 페이지 생성 (isLoggedIn 상태에 따라 /tabs로 리다이렉트)
    - RN 전체 레이아웃 개념 주석 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,14 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" />
+      {/* 커스텀아이징 할때만 여기에 추가해주면 된다 */}
       <Stack.Screen name="modal" options={{ presentation: "modal" }} />
     </Stack>
   );
 }
 // 전체 레이아웃에는 구지 화면 안넣더라도 구글 에널리틱스 or 초기화 or 권한 얻기 등 공통 레이아웃 있을 때 활용
 // Stack에서 Slot으로 변경하면 (tabs 부분이 사라진다)
+
+// home, Login, modal 이런얘들은 구지 안적어도 자동으로 추가되어 있다
+// 커스텀아이징 필요할 때만 presentation options 이런거 붙일때만 직접 추가해주면 된다.
+// 안적어주면 무시되는 것이 아니라, 자동으로 Expo Router가 파일기반으로 Stack의 일부구나하고 파악을 한다
